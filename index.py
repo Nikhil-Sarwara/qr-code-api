8546a4723df064c0093d2073342153547cc7738c
Nikhil <117506262+Nikhil-Sarwara@users.noreply.github.com>
2024-05-27 10:31:10 +0530

    make only one endpoint advanced for all features

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, send_file, jsonify
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
 import qrcode.image.styles.moduledrawers as md
@@ -38,44 +38,14 @@ def generate_qr_code():
         return jsonify({'error': str(e)}), 400
 
 # Color Customized QR Code
-# Default Color Blue
-@app.route('/api/color-customized/qr', methods=['POST'])
-def generate_color_customized_qr_code():
+@app.route('/api/advanced/qr', methods=['POST'])
+def generate_advanced_qr_code():
     try:
         data = request.json.get('text', '')
-        fill_color = request.json.get('fill_color', 'blue')
-        back_color = request.json.get('back_color', 'white')
-
-        if not data:
-            return jsonify({'error': 'No text provided'}), 400
-
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill_color=fill_color, back_color=back_color)
-        img_io = BytesIO()
-        img.save(img_io, 'PNG')
-        img_io.seek(0)
-
-        return send_file(img_io, mimetype='image/png')
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
-    
-# Styled QR Code
-@app.route('/api/styled/qr', methods=['POST'])
-def generate_styled_qr_code():
-    try:
-        data = request.json.get('text', '')
-        fill_color = request.json.get('fill_color', 'blue')
-        back_color = request.json.get('back_color', 'white')
+        fill_color = tuple(request.json.get('fill_color', [0, 0, 0]))
+        back_color = tuple(request.json.get('back_color', [255, 255, 255]))
         module_shape = request.json.get('module_shape', 'default')
+        eye_shape = request.json.get('eye_shape', 'default')
 
         if not data:
             return jsonify({'error': 'No text provided'}), 400
@@ -90,21 +60,38 @@ def generate_styled_qr_code():
         qr.add_data(data)
         qr.make(fit=True)
 
-        img = qr.make_image(fill_color=fill_color, back_color=back_color)
+        # Set module drawer
+        module_drawer = None
         if module_shape == 'rounded':
-            img = StyledPilImage(img, md.RoundedModuleDrawer())
+            module_drawer = md.RoundedModuleDrawer()
         elif module_shape == 'circle':
-            img = StyledPilImage(img, md.CircleModuleDrawer())
+            module_drawer = md.CircleModuleDrawer()
         elif module_shape == 'square':
-            img = StyledPilImage(img, md.SquareModuleDrawer())
+            module_drawer = md.SquareModuleDrawer()
         elif module_shape == 'gapped-square':
-            img = StyledPilImage(img, md.GappedSquareModuleDrawer())
+            module_drawer = md.GappedSquareModuleDrawer()
         elif module_shape == 'vertical-bars':
-            img = StyledPilImage(img, md.VerticalBarsDrawer())
+            module_drawer = md.VerticalBarsDrawer()
         elif module_shape == 'horizontal-bars':
-                img = StyledPilImage(img, md.HorizontalBarsDrawer())
-        else:
-            img = StyledPilImage(img, md.SquareModuleDrawer())
+            module_drawer = md.HorizontalBarsDrawer()
+
+        # Set eye drawer
+        eye_drawer = None
+        if eye_shape == 'rounded':
+            eye_drawer = md.RoundedModuleDrawer()
+        elif eye_shape == 'dots':
+            eye_drawer = md.CircleModuleDrawer()
+
+        # Generate the QR code image with the specified drawers and colors
+        img = qr.make_image(
+            image_factory=StyledPilImage,
+            module_drawer=module_drawer,
+            eye_drawer=eye_drawer,
+            color_mask=qrcode.image.styles.colormasks.SolidFillColorMask(
+                front_color=fill_color,
+                back_color=back_color
+            )
+        )
 
         img_io = BytesIO()
         img.save(img_io, 'PNG')
